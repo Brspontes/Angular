615729a932fe6b68aba0950f14ab958e18239d49
Brspontes <brian.robert16@hotmail.com>
2021-02-10 11:55:37 -0300

    criando novo component

--- Cod3r/frontend/src/app/app-routing.module.ts
+++ Cod3r/frontend/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './views/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   { path: "", component: HomeComponent },
+   { path: "produtos", component: ProductCrudComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
